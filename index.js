8e919873f763d6e0db0f86b0931c47f23907413a
Eleqtrick <87919560+Eleqtrick@users.noreply.github.com>
2022-08-24 01:21:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("generate-password").onclick = function () {
+   let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<,>.?/;:[{]}-_=+!@#$%^&*()~`";
+   let password = "";
+   let charsLength = chars.length;
+   let person = window.prompt("Enter a password length: ");
+   let num1 = parseInt(person);
+
+   for (var i = 0; i < num1; i++) {
+    let randNum = Math.floor(Math.random() * charsLength);
+    password += chars.substring(randNum, randNum + 1);
+
+    document.getElementById("password").value = password;
+   }
+}
